aff51d827b83c011709aae384d8e724ef6fe2699
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-03-27 19:47:57 +0200

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -52,3 +52,72 @@ new Swiper('.team-swiper',{
         
       },
 });
+
+new Swiper('.testimonials-swiper',{
+
+
+  navigation: {
+      nextEl: '.testimonials-arrow.swiper-button-next',
+      prevEl: '.testimonials-arrow.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+
+    
+  // direction: 'vertical',
+  // Перетаскивание на ПК
+  simulateTouch: true,
+  
+  // Чувствительность свайпа
+  touchRatio: 1,
+
+  // Угол срабатывания свайпа/перетаскивания
+  touchAngle: 45,
+
+  // Курсор перетаскивания
+  grabCursor: true,
+
+  // Переключение при клике на слайд
+  slideToClickedSlide: true,
+
+  // Навигация по хешу
+  hashNavigation: {
+      // отслеживать состояние
+      watchState: true,
+  },
+
+  // Управление клавиатурой
+  keyboard: {
+      // включить/выключить
+      enabled: true,
+
+      // Вкл/Выкл только когда слайдер в пределах вьюпорта
+      onlyInViewport: true,
+
+      // включить/выключить управление клавиатурой PageUp, pageDown
+      pageUpDown: true,
+  },
+  // Автовысота
+  autoHeihgt: true,
+  // Количство слайдов для показа
+  slidesPerView: 1,
+
+  breakpoints: {
+    320: {
+        slidesPerView: 1,
+    },
+    480: {
+        slidesPerView: 1,
+    },
+    992: {
+        slidesPerView: 1,
+    }
+}, 
+  // Отступ между слайдами
+  spaceBetween: 30,
+
+});
